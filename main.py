6d1839c6dd804264010a02ac015ced7bf84e043a
Your Name <you@example.com>
2020-03-23 23:37:41 +0900

    anysongs

---
+++ main.py
@@ -0,0 +1,82 @@
+k = int (input ("찾는 숫자를 입력하시오:"))
+
+data = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [-1, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24]
+]
+
+print(str(enumerate(data)))
+
+# for (index1,item1) in enumerate(data):
+#     for (index2,item2) in enumerate(item1):
+#         if(k==item2):
+#             print(index1,index2)
+#             print("찾았다")
+            
+
+
+# x = int (input('size : '))
+
+# if (x%2 != 0):
+#     for j in range(1,x):
+#         k=int (x/2)
+#         if(j <= x/2):   
+#             for z in range(k-j,0,-1):
+#                 print(" ",end="")
+#             for i in range(1,2*j):
+#                 print("*",end="")
+#         else:
+#             for h in range(j-k,0,-1):
+#                 print(" ",end="")
+#             for t in range(1,2*(x-j)):
+#                 print("*",end="")
+#         print()
+
+# else:
+#      print("탈락")
+
+
+
+
+# for j in range(1,6):
+#     #홀 인경우
+#     if(j%2 != 0):
+#         for i in range()
+
+#     else:
+
+    
+# for j in range(1,6):
+#     if(j <= 3):   
+#         for z in range(3-j,0,-1):
+#             print(" ",end="")
+#         for i in range(1,2*j):
+#             print("*",end="")
+#     else:
+#         for h in range(j-3,0,-1):
+#             print(" ",end="")
+#         for t in range(1,2*(6-j)):
+#             print("*",end="")
+#     print()
+
+
+# for j in range(1,5):
+#     for z in range(1,5-j):
+#         print("*",end="")
+#     for i in range(j,0,-1):
+#         print(i,end="")
+#     print()    
+    
+
+
+# for j in range (1,10):
+#     for z in range(1,10-j):
+#         print(" ",end='')
+#     for i in range (1,j+1):
+#         print(i,end="")
+#     print()
+
+
